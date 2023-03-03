50e51139ebc7cd95bc01f7abacb62375b4ad94fc
DDD120 <dltnals9794@naver.com>
2023-03-03 16:58:49 +0900

    feat 채팅 이모지 입력 구현

--- client/components/MessageInput.tsx
+++ client/components/MessageInput.tsx
@@ -1,25 +1,54 @@
-import { useRef } from "react";
+import { ChangeEvent, useState } from "react";
+import dynamic from "next/dynamic";
+import { EmojiClickData, EmojiStyle } from "emoji-picker-react";
+
+const EmojiPicker = dynamic(
+  () => {
+    return import("emoji-picker-react");
+  },
+  { ssr: false }
+);
 
 interface Props {
   onSetMessage: (message: string) => void;
 }
 
 export default function MessageInput({ onSetMessage }: Props) {
-  const inputRef = useRef<HTMLInputElement>(null);
+  const [content, setContent] = useState("");
+  const [isShowEmojiPicker, setIsShowEmojiPicker] = useState(false);
 
-  const handleClick = () => {
-    const message = inputRef.current?.value;
-    if (!message) {
+  const handleSendClick = () => {
+    if (!content) {
       return;
     }
-    onSetMessage(message);
-    inputRef.current.value = "";
+    onSetMessage(content);
+    setContent("");
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setContent(e.target.value);
+  };
+
+  const handleEmojiPickerClick = () => {
+    setIsShowEmojiPicker(true);
+  };
+
+  const handleEmojiClick = ({ emoji }: EmojiClickData) => {
+    setContent((prev) => prev + emoji);
+    setIsShowEmojiPicker(false);
   };
 
   return (
     <div>
-      <input type="text" ref={inputRef} />
-      <button onClick={handleClick}>확인</button>
+      <input type="text" value={content} onChange={handleInputChange} />
+      <button onClick={handleEmojiPickerClick}>이모지</button>
+      {isShowEmojiPicker && (
+        <EmojiPicker
+          onEmojiClick={handleEmojiClick}
+          emojiStyle={EmojiStyle.GOOGLE}
+        />
+      )}
+      <button onClick={handleSendClick}>확인</button>
     </div>
   );
 }
