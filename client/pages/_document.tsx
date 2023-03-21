0dc4743890052f9869cdd85afbe8a3a328ce6be9
DDD120 <dltnals9794@naver.com>
2023-03-21 14:27:58 +0900

    fix room 페이지 화면 축소 시 요소 짤림 해결

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="ko" className="h-full">
       <Head />
-      <body className="bg-navy-900 min-w-[1288px] min-h-[600px] text-white h-full flex justify-center items-center font-maple-light">
+      <body className="bg-navy-900 h-full text-white flex justify-center items-center font-maple-light">
         <Main />
         <NextScript />
       </body>
--- client/pages/room.tsx
+++ client/pages/room.tsx
@@ -5,7 +5,7 @@ import { RoomProvider } from "@/store/room";
 export default function Room() {
   return (
     <RoomProvider>
-      <div className="flex">
+      <div className="min-w-[1288px] min-h-[600px] h-full flex justify-center items-center">
         <Canvas />
         <Panel />
       </div>
