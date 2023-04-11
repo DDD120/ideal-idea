61555b0b85585b1f2ea7f37f1a54026b48adc89a
DDD120 <dltnals9794@naver.com>
2023-04-11 14:37:28 +0900

    fix 이미지 설정 수정

--- client/components/room/Panel.tsx
+++ client/components/room/Panel.tsx
@@ -10,7 +10,7 @@ export default function Panel() {
     <section className="bg-navy-800 w-72 h-[600px] rounded-sm flex flex-col justify-between">
       <h1 className="text-white px-3 pt-3 flex items-center gap-2">
         <div className="w-4">
-          <Image src="/logo.png" alt="로고" width={72} height={72} />
+          <Image src="/logo.png" alt="ideal idea 로고" width="72" height="72" />
         </div>
         ideal idea
       </h1>
--- client/next.config.js
+++ client/next.config.js
@@ -9,10 +9,6 @@ const nextConfig = {
     });
     return config;
   },
-  images: {
-    loader: "akamai",
-    path: "/",
-  },
 };
 
 module.exports = nextConfig;
