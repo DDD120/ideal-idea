1e57f309441a3d7727e0b06615035ef03168c03a
DDD120 <dltnals9794@naver.com>
2023-04-18 22:48:31 +0900

    feat 서버 루트 페이지 추가

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RoomGateway } from './room/room.gateway';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  providers: [RoomGateway],
+  providers: [AppService, RoomGateway],
+  controllers: [AppController],
 })
 export class AppModule {}
